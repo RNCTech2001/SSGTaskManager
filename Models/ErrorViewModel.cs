4933ed909ba2690f68816744a2081d7aa89442bf
RNC <56437977+RNCTech2001@users.noreply.github.com>
2023-06-16 12:04:12 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSGTaskManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
