27c0a03c6385f694c707200b3acce767c9e1f3c8
RNC <56437977+RNCTech2001@users.noreply.github.com>
2023-06-16 12:17:19 +0800

    Add ETC_Inventory.cs
    Class for ETC_Inventory

---
+++ Models/ETC_Inventory.cs
@@ -0,0 +1,16 @@
+﻿namespace SSGTaskManager.Models
+{
+    public class ETC_Inventory
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public string Date { get; set; }
+        public string Name { get; set; }
+
+        public ETC_Inventory()
+        {
+            
+        }
+
+    }
+}
